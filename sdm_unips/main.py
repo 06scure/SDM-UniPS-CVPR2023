1ea2cf5757fe0c6ebf01b9776274d8ad022f5631
06scure <937416937@qq.com>
2026-04-09 14:58:29 +0800

    跑通SDM的推理部分

--- sdm_unips/main.py
+++ sdm_unips/main.py
@@ -1,9 +1,3 @@
-"""
-Scalable, Detailed and Mask-free Universal Photometric Stereo Network (CVPR2023)
-# Copyright (c) 2023 Satoshi Ikehata
-# All rights reserved.
-"""
-
 from __future__ import print_function, division
 from modules.model.model_utils import *
 from modules.builder import builder
@@ -19,15 +13,15 @@ parser = argparse.ArgumentParser()
 
 # Properties
 parser.add_argument('--session_name', default='sdm_unips')
-parser.add_argument('--target', default='normal_and_brdf', choices=['normal', 'brdf', 'normal_and_brdf'])
-parser.add_argument('--checkpoint', default='checkpoint')
+parser.add_argument('--target', default='normal', choices=['normal', 'brdf', 'normal_and_brdf'])
+parser.add_argument('--checkpoint', default='/home/user/dataset/ckpt/sdm_unips/')
 
 # Data Configuration
 parser.add_argument('--max_image_res', type=int, default=4096)
 parser.add_argument('--max_image_num', type=int, default=10)
-parser.add_argument('--test_ext', default='.data')
-parser.add_argument('--test_dir', default='DefaultTest')
-parser.add_argument('--test_prefix', default='L*')
+parser.add_argument('--test_ext', default='PNG')
+parser.add_argument('--test_dir', default='/home/user/dataset/DiLiGenT_518')
+parser.add_argument('--test_prefix', default='[0-9]*')
 parser.add_argument('--mask_margin', type=int, default=8)
 
 # Network Configuration
@@ -52,8 +46,10 @@ def main():
     end_time = time.time()
     print(f"Prediction finished (Elapsed time is {end_time - start_time:.3f} sec)")
     print("\nExecute the following script to render a video under new lighting conditions based on the generated BRDF and normal map.\n")
-    print(f"        python sdm_unips/relighting.py --datadir ./{args.session_name}/results/{test_data.data.objname}\n")
-         
+    import os
+    objname = os.path.basename(test_data.objlist[0]) if len(test_data.objlist) > 0 else ""
+    print(f"        python sdm_unips/relighting.py --datadir ./{args.session_name}/results/{objname}\n")
+        
 
 
 if __name__ == '__main__':
